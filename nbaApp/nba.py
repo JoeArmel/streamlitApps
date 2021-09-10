73222d3747bd383b894a926c8431aa320a7000a7
Armel Momo <miatsuet@gmail.com>
2021-09-10 14:38:55 +0200

    modif1

--- nbaApp/nba.py
+++ nbaApp/nba.py
@@ -68,7 +68,7 @@ st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 if st.button('Matrice Heatmap'):
     st.header('Matrice Heatmap d\'Inter-corrélation')
     df_selected_team.to_csv('output.csv', index=False)
-    df = pd.read_csv('output.csv')
+    df = pd.read_csv('../USfootballApp/output.csv')
 
     corr = df.corr()
     mask = np.zeros_like(corr)
