f311e436465d4e562d86911ecf7409b8d4b734e0
JoeArmel <77776974+JoeArmel@users.noreply.github.com>
2022-06-15 19:10:41 +0200

    Update SP500.py

--- sp500App/SP500.py
+++ sp500App/SP500.py
@@ -7,10 +7,10 @@ import yfinance as yf
 
 # Mise en page
 # Division de l'entête en deux colonnes. Une pour le logo et l'autre pour le titre
-col1, col2 = st.columns((2, 6))
+col1, col2 = st.columns((2.5, 6))
 
 image = Image.open('sp500App/sp500logo.jpg')
-col1.image(image, width=200, output_format='auto')
+col1.image(image, width=180, output_format='auto')
 col2.title('Standard and Poor\'s 500 App')
 #st.title('Standard and Poor\'s 500 App')
 
